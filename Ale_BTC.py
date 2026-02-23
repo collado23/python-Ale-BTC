1c1e2bb9b7b301d894af2ae585322087753b6ccc
collado23 <collado23aaa@gmail.com>
2026-02-22 21:56:20 -0300

    Update Ale_BT.py

--- Ale_BTC.py
+++ Ale_BTC.py
@@ -1,4 +1,4 @@
-import os, time, threading, datetime
+import os, time, threading, datetime 
 from http.server import BaseHTTPRequestHandler, HTTPServer 
 from binance.client import Client
 from binance.enums import *
